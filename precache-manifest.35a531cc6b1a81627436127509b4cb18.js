0e3ecbb9924d06bf026ebc5ed6bb376b773d057d
kcone09 <kcone09@gmail.com>
2020-06-13 11:48:37 +0200

    added relaxer app and changed weather app url

---
+++ precache-manifest.35a531cc6b1a81627436127509b4cb18.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a82a7d1c1fad59a2a5dc1b9d7f5802e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca740af4b0067b353c54",
+    "url": "/static/css/main.27a7e40b.chunk.css"
+  },
+  {
+    "revision": "facc9bc9bcc8456e4365",
+    "url": "/static/js/2.36c37ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36c37ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca740af4b0067b353c54",
+    "url": "/static/js/main.0555784c.chunk.js"
+  },
+  {
+    "revision": "7b41a341eda0a090ec3e",
+    "url": "/static/js/runtime-main.0996e8ac.js"
+  },
+  {
+    "revision": "a618532dd99e8ff35235279b3bca0775",
+    "url": "/static/media/Enock.a618532d.png"
+  },
+  {
+    "revision": "d2ca80d7a1d88845a86cf17feb075991",
+    "url": "/static/media/blue_circle.d2ca80d7.svg"
+  },
+  {
+    "revision": "e8d3f3f104697b7e6b66a29c90aec808",
+    "url": "/static/media/green_rectangle.e8d3f3f1.svg"
+  },
+  {
+    "revision": "2ce40770b03dcea57a5d0c206dab9978",
+    "url": "/static/media/grey_rounded_rectangle.2ce40770.svg"
+  },
+  {
+    "revision": "4d6d0f8a7910f7616995c14099ace695",
+    "url": "/static/media/red_triangle.4d6d0f8a.svg"
+  },
+  {
+    "revision": "8a2e5b567e4e97a6d5869c781c8e7ed8",
+    "url": "/static/media/yellow_dots.8a2e5b56.svg"
+  },
+  {
+    "revision": "7d79e22a92d1225a922dbd81763c5f27",
+    "url": "/static/media/yellow_semicircle.7d79e22a.svg"
+  }
+]);
